02a8dc04e253108791e3a147c57ffbf789c954b6
Trang Hoang <nyccowgirl@TrangHoangsMBP.att.net>
2017-04-25 23:41:50 -0700

    Corrected code for input of birthday and start code for single digit (in notes)

--- numerology.py
+++ numerology.py
@@ -2,7 +2,6 @@ while True:
     welcome = raw_input("Welcome to the world of numerology. Do you want to learn more about your core numbers? (Y/N) ")
     if welcome.lower() == "n":
         print "You're missing out on the path to enlightenment. Have a great day!"
-        break
     elif welcome.lower() != "y":
         print "Input Y or N"
     else:
@@ -20,12 +19,21 @@ while True:
                         if not day.isdigit():
                             print "Please input the numerical value of the day."
                         else:
-                            if (month in (1, 3, 5, 7, 8, 10, 12)) and ((day < 1) or (day > 31)):
+                            day = int(day)
+                            if (month in [1, 3, 5, 7, 8, 10, 12]) and ((day < 1) or (day > 31)):
                                 print "That may be a day on Jupiter, but not on Earth!"
                             elif (month == 2) and ((day < 1) or (day > 29)):
                                 print "That may be a day on Venus, but not on Earth!"
-                            elif (month in (4, 6, 9, 11)) and ((day < 1) or (day > 30)):
+                            elif (month in [4, 6, 9, 11]) and ((day < 1) or (day > 30)):
                                 print "That may be a day on Saturn, but not on Earth!"
                             else:
                                 while True:
                                     year = raw_input("Input your year of birth: ")
+                                    if not year.isdigit():
+                                        print "Please input the year based on the Gregorian calendar."
+                                    else:
+                                        year = int(year)
+                                    break
+                        break
+            break
+    break
